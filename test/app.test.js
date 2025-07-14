3bd0fb45ee0c475d833553ca22ee512000eccee6
Angel <angelarturogarciarodriguez055@gmail.com>
2025-07-13 23:38:03 -0600

    Primer commit

---
+++ test/app.test.js
@@ -0,0 +1,10 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+describe('GET /', () => {
+  it('should return HTML', async () => {
+    const res = await request(app).get('/');
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toContain('Práctica de la clase Lenguajes');
+  });
+});
